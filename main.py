1498eca5e535a5082bb30451f2f68753f0a171c2
tizianatocci <tiziana.tocci96@hotmail.it>
2025-11-10 15:59:23 +0100

    correct main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.datascienceproject import logger
-from datascienceproject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascienceproject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.initiate_data_ingestion()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.expection(e)
