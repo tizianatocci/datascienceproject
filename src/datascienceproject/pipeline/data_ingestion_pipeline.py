32570ead431d393ddd4ff79b31d9324d76ed16f5
tizianatocci <tiziana.tocci96@hotmail.it>
2025-11-10 16:19:17 +0100

    correct file

--- src/datascienceproject/pipeline/data_ingestion_pipeline.py
+++ src/datascienceproject/pipeline/data_ingestion_pipeline.py
@@ -15,7 +15,7 @@ class DataIngestionTrainingPipeline:
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
 
-if __name__ == 'main':
+if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
