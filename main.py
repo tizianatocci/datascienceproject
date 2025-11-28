1ea99c3c8718feb35c92c9e17b681da49081b994
tizianatocci <tiziana.tocci96@hotmail.it>
2025-11-28 18:05:30 +0100

    completed data validation pipeline implementation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.datascienceproject import logger
 from src.datascienceproject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascienceproject.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -7,6 +8,16 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.expection(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataValidationTrainingPipeline()
+    data_ingestion.initiate_data_validation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
     logger.expection(e)
